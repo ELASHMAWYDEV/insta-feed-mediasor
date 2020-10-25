37fd007bae3d5a42a03a28e3ca4e16d94eff330e
ELASHMAWY DEV <contact@elashmawydev.com>
2020-10-25 17:11:06 +0200

    Added react app configuration to index.js

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const PORT = process.env.PORT || 5000;
 
 //IGQVJWSXRIZAExlWEZAYM2t3cjc5VklOOU9FaGZAjOEpKRkI4ZAHFLbDFnSFBXRmwwNHBBa1pLcWJQRE1QeWhpaU5HWTRyQWh6a2M1bEpEejlPS29mSENybGdlM2tqMVNWSDZAmMmhDbTBsUWdqTDF4ZAnpxQQZDZD
 
+
+//For the react app
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
