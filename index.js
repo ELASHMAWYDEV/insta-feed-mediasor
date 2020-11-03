44a107edc2477cdda90bf44eb7cb9a5649522b69
ELASHMAWY DEV <contact@elashmawydev.com>
2020-11-03 13:09:25 +0200

    Added puppeteer to scrape instagram profile

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//IGQVJWSXRIZAExlWEZAYM2t3cjc5VklOOU9FaGZAjOEpKRkI4ZAHFLbDFnSFBXRmwwNHBBa1pLcWJQRE1QeWhpaU5HWTRyQWh6a2M1bEpEejlPS29mSENybGdlM2tqMVNWSDZAmMmhDbTBsUWdqTDF4ZAnpxQQZDZD
-
 
 //For the react app
 if (process.env.NODE_ENV == "production") {
