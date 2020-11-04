b6fb0b783fc6e7a16202ec7c93a8fafad83369a4
ELASHMAWY DEV <contact@elashmawydev.com>
2020-11-04 21:32:44 +0200

    Added puppeteer, scraped all posts links

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+const scraper = require("./scraper");
 
 
 //For the react app
@@ -10,6 +11,16 @@ if (process.env.NODE_ENV == "production") {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
+
+(async () => {
+  try {
+
+    let data = await scraper();
+    // console.log(data);
+  } catch (e) {
+    console.log(e.message);
+  }
+})();
 
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
--- scraper.js
+++ scraper.js
@@ -1,5 +1,63 @@
 const puppeteer = require("puppeteer");
+const { performance } = require('perf_hooks');
 
-module.exports = {
-  
-}
+module.exports = async (username = "mediasor") => {
+  try {
+    //Initialize vars
+    let data = {};
+
+    //Calculate time for operation
+    let t0 = performance.now();
+
+    //Scraping Init
+    let browser = await puppeteer.launch();
+    let page = await browser.newPage();
+
+    //Open the user profile page
+    await page.goto(`https://www.instagram.com/${username}`);
+
+    //Get user profile info
+    const id = await page.$$eval(".-vDIg > a", links => links.map(a => a.getAttribute("author_id"))[0]);
+    const bio = await page.evaluate(() => document.querySelector(".-vDIg > span").textContent);
+    const followers = await page.evaluate(() => document.querySelector("span.g47SY").textContent)
+    const fullName = await page.evaluate(() => document.querySelector("h1.rhpdm").textContent)
+    const profilePic = await page.evaluate(() => document.querySelector("img._6q-tv").src)
+    const postsCount = await page.evaluate(() => document.querySelector("span.g47SY").textContent)
+
+
+    /*-------------...Get Posts...------------*/
+    let postsLinks = new Set();
+
+    //Scroll to the end first
+    while (postsLinks.size < postsCount) {
+
+      try {
+        //Get the visible posts links
+        const nodes = await page.$$eval("div.v1Nh3.kIKUG._bz0w > a", links => links.map(a => a.href));
+        
+        //Scroll to end
+        await page.evaluate(`window.scrollTo(0, document.body.scrollHeight + 1000)`);
+        
+        
+        //Add nodes to the posts set
+        nodes.forEach(post => {
+          postsLinks.add(post);
+        });
+      } catch (e) {
+        console.log(e.message);
+      }
+    }  
+
+    await browser.close();
+
+    //Log taken time to execute
+    let t1 = performance.now();
+    console.log(`Time taken: ${((t1 - t0) / 1000).toFixed(1)}s`);
+
+    return data;
+
+  } catch (e) {
+
+    console.log(e.message);
+  }
+};
