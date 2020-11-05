2e824441e33e9141ce8a9813be9e515422708804
ELASHMAWY DEV <contact@elashmawydev.com>
2020-11-05 14:48:24 +0200

    Grabbing posts data & posts comments...Done

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
-const { performance } = require('perf_hooks');
+const { performance } = require("perf_hooks");
+const axios = require("axios");
 
 module.exports = async (username = "mediasor") => {
   try {
@@ -9,44 +10,22 @@ module.exports = async (username = "mediasor") => {
     //Calculate time for operation
     let t0 = performance.now();
 
-    //Scraping Init
+    //Init browser
     let browser = await puppeteer.launch();
     let page = await browser.newPage();
+    await page.goto(`https://www.instagram.com/${username}`); //Open the user profile page
 
-    //Open the user profile page
-    await page.goto(`https://www.instagram.com/${username}`);
+    //Get profile info
+    const { id, bio, followers, fullName, profilePic, postsCount } =
+      (await getProfileInfo(username)) || {};
 
-    //Get user profile info
-    const id = await page.$$eval(".-vDIg > a", links => links.map(a => a.getAttribute("author_id"))[0]);
-    const bio = await page.evaluate(() => document.querySelector(".-vDIg > span").textContent);
-    const followers = await page.evaluate(() => document.querySelector("span.g47SY").textContent)
-    const fullName = await page.evaluate(() => document.querySelector("h1.rhpdm").textContent)
-    const profilePic = await page.evaluate(() => document.querySelector("img._6q-tv").src)
-    const postsCount = await page.evaluate(() => document.querySelector("span.g47SY").textContent)
+    //Get links
+    let postsLinks = await getLinks(page, postsCount);
 
+    //Get each post data
+    let posts = await getPostsData(postsLinks);
 
-    /*-------------...Get Posts...------------*/
-    let postsLinks = new Set();
-
-    //Scroll to the end first
-    while (postsLinks.size < postsCount) {
-
-      try {
-        //Get the visible posts links
-        const nodes = await page.$$eval("div.v1Nh3.kIKUG._bz0w > a", links => links.map(a => a.href));
-        
-        //Scroll to end
-        await page.evaluate(`window.scrollTo(0, document.body.scrollHeight + 1000)`);
-        
-        
-        //Add nodes to the posts set
-        nodes.forEach(post => {
-          postsLinks.add(post);
-        });
-      } catch (e) {
-        console.log(e.message);
-      }
-    }  
+    console.log(posts);
 
     await browser.close();
 
@@ -55,9 +34,114 @@ module.exports = async (username = "mediasor") => {
     console.log(`Time taken: ${((t1 - t0) / 1000).toFixed(1)}s`);
 
     return data;
-
   } catch (e) {
-
     console.log(e.message);
+  }
+};
+
+const getProfileInfo = async (username) => {
+  //Get the user object from __a=1
+  let response = await axios.get(`https://www.instagram.com/${username}?__a=1`);
+  let data = await response.data;
+
+  let profileInfo = {
+    id: data.graphql.user.id,
+    bio: data.graphql.user.biography,
+    followers: data.graphql.user.edge_followed_by.count,
+    fullName: data.graphql.user.full_name,
+    profilePic: data.graphql.user.profile_pic_url_hd,
+    postsCount: data.graphql.user.edge_owner_to_timeline_media.count,
+  };
+
+  return profileInfo;
+};
+
+//Get all posts links
+const getLinks = async (page, postsCount) => {
+  /*-------------...Get Posts' Links...------------*/
+  let postsLinks = new Set();
+
+  //Scroll to the end
+  while (postsLinks.size < postsCount) {
+    try {
+      //Get the visible posts links
+      const nodes = await page.$$eval("div.v1Nh3.kIKUG._bz0w > a", (links) =>
+        links.map((a) => a.href)
+      );
+
+      //Scroll to end
+      await page.evaluate(
+        `window.scrollTo(0, document.body.scrollHeight + 1000)`
+      );
+
+      //Add nodes to the posts set
+      nodes.forEach((post) => {
+        postsLinks.add(post);
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
+  return [...postsLinks];
+};
+
+const getPostsData = async (postsLinks) => {
+  try {
+    //Init empty object
+    let postsData = [];
+
+    //Open new tab in puppeteer & open post link
+    for (let link of postsLinks) {
+      const response = await axios.get(`${link}?__a=1`);
+      const data = await response.data;
+
+      //Get neccessary data only
+      let postData = {
+        id: data.graphql.shortcode_media.id,
+        type: data.graphql.shortcode_media.__typename,
+        url: `https://instagram.com/p/${data.graphql.shortcode_media.shortcode}`,
+        imageUrl: data.graphql.shortcode_media.display_url,
+        isVideo: data.graphql.shortcode_media.is_video,
+        viewsCount: data.graphql.shortcode_media.video_view_count || null,
+        caption:
+          data.graphql.shortcode_media.edge_media_to_caption.edges[0].node.text,
+        commentsCount:
+          data.graphql.shortcode_media.edge_media_to_parent_comment.count,
+        commentsDisabled: data.graphql.shortcode_media.comments_disabled,
+        timestamp: data.graphql.shortcode_media.taken_at_timestamp,
+        likesCount: data.graphql.shortcode_media.edge_media_preview_like.count,
+        comments: data.graphql.shortcode_media.edge_media_to_parent_comment.edges.map(
+          ({ node }) => {
+            return {
+              id: node.id,
+              text: node.text,
+              timestamp: node.created_at,
+              username: node.owner.username,
+              imageUrl: node.owner.profile_pic_url,
+              likesCount: node.edge_liked_by.count,
+              repliesCount: node.edge_threaded_comments.count,
+              replies: node.edge_threaded_comments.edges.map(({ node }) => {
+                return {
+                  id: node.id,
+                  text: node.text,
+                  timestamp: node.created_at,
+                  username: node.owner.username,
+                  imageUrl: node.owner.profile_pic_url,
+                  likesCount: node.edge_liked_by.count,
+                };
+              }),
+            };
+          }
+        ),
+      };
+
+      postsData.push(postData);
+    }
+
+    return postsData;
+  } catch (e) {
+    console.log(e);
+    return [];
   }
 };
