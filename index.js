9f8e2fbe5d8ea418b2d6bfe97f06ff6c1bb8b1fb
ELASHMAWY DEV <contact@elashmawydev.com>
2020-11-06 21:01:42 +0200

    Fixed 'path' error on index.js for react build

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const bodyParser = require("body-parser");
+const path = require("path");
 const cors = require("cors");
 const PORT = process.env.PORT || 5001;
 
