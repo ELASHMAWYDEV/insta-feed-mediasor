2918f3334d78e14c7691f9bdf5789f162bf8dd1c
ELASHMAWY DEV <contact@elashmawydev.com>
2020-11-07 09:33:04 +0200

    minor fixs to config file: added env for vars

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 const { hostname, port, protocol } = window.location;
 
-export const SOCKET_URL = `${protocol == "https" ? "wss" : "ws"}://${hostname}:${process.env.NODE_ENV == "production" ? port : 5001}`;
+export const SOCKET_URL = `${protocol === "https" ? "wss" : "ws"}://${hostname}:${process.env.NODE_ENV === "production" ? port : 5001}`;
